80ed9b3663389a1eaadfb0bab421118c7beee50d
sikor <siegfriedek@gmail.com>
2013-11-09 13:10:09 +0100

    Changed DB Path to relative one. Added twitdb to .gitignore.

--- TwitApp/settings.py
+++ TwitApp/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/sg0220017/Desktop/tai/twitdb',                      # Or path to database file if using sqlite3.
+        'NAME': 'twitdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
